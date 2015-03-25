760c8a291c32d5ca5f920742463fe8248185489c
dariuszp <poltorak.dariusz@gmail.com>
2015-03-25 13:34:35 +0100

    logs

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 'use strict';
 
 var express = require('express'),
+    morgan = require('morgan'),
+    fs = require('fs'),
     app = express();
+
+app.use(morgan('combined', {
+    stream: fs.createWriteStream(__dirname + '/logs/access.log', { flags: 'a' })
+}));
+
+app.use(morgan('combined', {
+    skip: function (req, res) { return res.statusCode < 400 },
+    stream: fs.createWriteStream(__dirname + '/logs/error.log', { flags: 'a' })
+}));
 
 app.get('/', function (req, res) {
     res.end('Hello world');
+});
+
+app.get('/error', function (req, res) {
+    throw new Error('This is error');
+    res.end('Hello error');
 });
 
 app.listen(3000, function () {
